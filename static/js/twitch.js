899351c52b9c12f4dd10a8accdc9470abdd3a39c
Alex Brooke <akb.sbc@gmail.com>
2020-08-31 22:10:46 -0400

    add poolside xtreme schedule

--- static/js/twitch.js
+++ static/js/twitch.js
@@ -1,7 +1,18 @@
 import "./vendor/twitch-embed-v1.min.js";
+import "./vendor/twitch-embed-v1.min.js";
 
-new Twitch.Embed("twitch-embed", {
+const player = new Twitch.Embed("twitch-embed", {
     width: "100%",
     height: "100%",
     channel: "Team_Poolside"
 });
+
+const setStreamLinkVisible = (visible = true) => () => {
+    console.log('setting visibility', visible);
+    // document.getElementById("watch-on-twitch").style.visibility = visible ?
+    //     "visible" : "hidden";
+    document.getElementById("stream-live").innerText = `Currently ${visible ? "ONLINE" : "OFFLINE"}`;
+};
+
+player.addEventListener(Twitch.Player.ONLINE, setStreamLinkVisible());
+player.addEventListener(Twitch.Player.OFFLINE, setStreamLinkVisible(false));
