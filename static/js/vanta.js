60873bec9b6e06b25043a0aff62b9b6deb81574a
Alex Brooke <akb.sbc@gmail.com>
2020-12-24 01:25:13 -0500

    update theme for yuleside

--- static/js/vanta.js
+++ static/js/vanta.js
@@ -4,6 +4,8 @@ import "./vendor/vanta.waves.min.js";
 
 let effect = null;
 
+// old color: 0x005182
+
 export const toggleBackgroundEffect = () => {
     if (!effect) {
         effect = VANTA.WAVES({
@@ -15,7 +17,7 @@ export const toggleBackgroundEffect = () => {
             minWidth: 200.00,
             scale: 1.00,
             scaleMobile: 1.00,
-            color: 0x5182,
+            color: 0x6e001a,
             shininess: 27.00,
             waveHeight: 21.50,
             waveSpeed: 0.90
